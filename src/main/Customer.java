f76305ab43386bac2a38a1bd65dda87a1c8025e7
koehler1000 <koehler9@gmail.com>
2018-05-13 18:01:53 +0200

    parameterized use of vector

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private Vector<Rental> rentals = new Vector<Rental>();
     public Customer (String newname){
         name = newname;
     };
@@ -18,7 +18,7 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration<Rental> enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
