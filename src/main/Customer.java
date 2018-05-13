73e2223744d556364a20c8d46a2ee7460d2f1574
koehler1000 <koehler9@gmail.com>
2018-05-13 18:10:21 +0200

    removing unnecessary semicolons

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -8,13 +8,13 @@ public class Customer {
     private Vector<Rental> rentals = new Vector<Rental>();
     public Customer (String newname){
         name = newname;
-    };
+    }
     public void addRental(Rental arg) {
         rentals.addElement(arg);
-    };
+    }
     public String getName (){
         return name;
-    };
+    }
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
