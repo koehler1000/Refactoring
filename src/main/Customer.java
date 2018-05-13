0672bb8cedee8e079b2e8c3c444e507c3c33ee70
koehler1000 <koehler9@gmail.com>
2018-05-13 18:34:24 +0200

    removed ValueOf

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -33,7 +33,7 @@ public class Customer {
             if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
